dee38fb13f84e50cb90c223c4535fde2e9fd2e9f
dennismolund <43784882+dennismolund@users.noreply.github.com>
2019-03-12 14:38:33 +0100

    ShowEvent, visar upp events. kod för skapa events för läggas till i ShowEvent classen under doneButton.Clicklistener.

--- app/src/main/java/mode1719/student/ju/jade/ShowEvent.java
+++ app/src/main/java/mode1719/student/ju/jade/ShowEvent.java
@@ -15,52 +15,54 @@ import org.w3c.dom.Text;
 
 public class ShowEvent extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_event);
 
-        checkParentActivity();
+        checkActivity();
 
 
     }
 
-    public void checkParentActivity(){
+    public void checkActivity(){
+        Button doneButton = findViewById(R.id.doneButton);
+        ImageView eventImage = findViewById(R.id.eventImage);
+        TextView eventTitle = findViewById(R.id.eventTitle);
+        TextView eventTime = findViewById(R.id.eventTime);
+        TextView eventDescription= findViewById(R.id.eventDescription);
+
+
         Intent intent = getIntent();
         int value = intent.getIntExtra("value", 0);
         if (value == 1){
-            Button doneButton = findViewById(R.id.doneButton);
             doneButton.setVisibility(View.GONE);
-            getIntents();
+
+
+            String imageUrl = getIntent().getStringExtra("image_url");
+            String title = getIntent().getStringExtra("title");
+            String time = getIntent().getStringExtra("time");
+            String description = getIntent().getStringExtra("description");
+
+            Glide.with(this).asBitmap().load(imageUrl).into(eventImage);
+            eventTitle.setText(title);
+            eventTime.setText(time);
+            eventDescription.setText(description);
+
+        }
+
+        else {
+            doneButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //add to database
+                    finish();
+                }
+            });
         }
 
     }
-
-    private void getIntents(){
-
-        String imageUrl = getIntent().getStringExtra("image_url");
-        String title = getIntent().getStringExtra("title");
-        String time = getIntent().getStringExtra("time");
-        String description = getIntent().getStringExtra("description");
-
-        setEvent(imageUrl,title,time,description);
-    }
-
-     private void setEvent(String imageUrl, String title, String time, String description ){
-        ImageView eventImage = findViewById(R.id.eventImage);
-        Glide.with(this).asBitmap().load(imageUrl).into(eventImage);
-
-        TextView eventTitle = findViewById(R.id.eventTitle);
-        eventTitle.setText(title);
-
-        TextView eventTime = findViewById(R.id.eventTime);
-        eventTime.setText(time);
-
-        TextView eventDescription= findViewById(R.id.eventDescription);
-        eventDescription.setText(description);
-
-
-
-
-    }
 }
+
