486d337f5a934918f7fbdea7a5af3063d4c95b01
Jake <jacob.carlquist@hotmail.com>
2019-03-17 17:57:29 +0100

    Remade showEvent to success

--- app/src/main/java/mode1719/student/ju/jade/ShowEvent.java
+++ app/src/main/java/mode1719/student/ju/jade/ShowEvent.java
@@ -2,7 +2,10 @@ package mode1719.student.ju.jade;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
@@ -24,10 +27,13 @@ import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
+import java.io.IOException;
 import java.util.Date;
 
 public class ShowEvent extends AppCompatActivity {
 
+    private static final int PICK_IMAGE = 1;
+    public Uri imageUri;
     public EditText eventDescription;
     public EditText eventTime;
     public ImageView eventImage;
@@ -45,12 +51,38 @@ public class ShowEvent extends AppCompatActivity {
         else if(getIntentVal() == 0){
             createNewEvent();
         }
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK){
+            imageUri = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                eventImage.setImageBitmap(bitmap);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 
     public void createNewEvent(){
         Button doneBtn = findViewById(R.id.doneButton);
         doneBtn.setVisibility(View.VISIBLE);
         final Date date = getDateIntent();
+
+        eventImage = findViewById(R.id.eventImage);
+        eventImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gallery = new Intent();
+                gallery.setType("image/*");
+                gallery.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(gallery, "Select image"), PICK_IMAGE);
+            }
+        });
         doneBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
